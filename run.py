2db467479621fdea6aa7cf8196a26e0de909ee0e
johnrm <notjohnrm@gmail.com>
2024-05-08 18:44:05 +0000

    Add menu options

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import getpass
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -19,20 +20,34 @@ data = accounts.get_all_values()
 print(data)
 x = input("<Enter> to begin\n")
 
+def validate_card():
+    card = input("Insert card or enter card ID to begin: ")
+    password=getpass.getpass('PIN (4 digits): ')
+    #print(password)
+#    a=input ("Press <Enter>")
+    return True
+    
+def menu():
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("        Reptilia Bank")
+        print("-----------------------------")
+        print("         ATM Options")
+        print("-----------------------------")
+        print("1> Check Balance")
+        print("2> Withdraw cash")
+        print("3> Lodgement")
+        print("4> Print Statement\n")
+        choice=input("Select: ")
+
 
 def main():
-    os.system('cls' if os.nam  == 'nt' else 'clear')
-    print("   Reptilia Bank")
-    print("-------------------")
-    print("        ATM")
-    print("-------------------\n")
-  
-    card = input("Insert card or enter card ID to begin\n")
-    print("    ATM Options")
-    print("-------------------\n")
-    print("1> Check Balance")
-    print("2> Withdrawal")
-    print("3> Lodgement")
-    print("4> Print Statement\n")
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("        Reptilia Bank")
+        print("-----------------------------")
+        print("             ATM")
+        print("-----------------------------\n")
+        if validate_card():
+            menu()
 
 main()
